2162b0173bfb1c9d1c0e18d72b75558271c8f171
Anton <icsxakep@gmail.com>
2023-12-09 22:54:52 +0300

    Add total_amount

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -134,6 +134,7 @@ class OrderModel(models.Model):
     items = models.ManyToManyField('ItemModel', related_name='order_set')
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
     no_call_back = models.BooleanField(default=False, verbose_name="Не передзвонювати")
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
     def __str__(self):
         return f'OrderModel {self.id} by {self.first_name} {self.last_name}'
