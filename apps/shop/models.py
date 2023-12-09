f19d9047cdf694b56fbe58e6a4119311829300ff
Anton <icsxakep@gmail.com>
2023-12-09 22:58:29 +0300

    Rename item_set

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -131,7 +131,7 @@ class OrderModel(models.Model):
     comment = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
-    items = models.ManyToManyField('ItemModel', related_name='order_set')
+    item_set = models.ManyToManyField('ItemModel', related_name='order_set')
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
     no_call_back = models.BooleanField(default=False, verbose_name="Не передзвонювати")
     total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
