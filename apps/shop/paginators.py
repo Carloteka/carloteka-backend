d94a313fe38b81979120e8ec92c3d4b1bc7f8501
AlexUA_ubuntu <alex2011ua@gmail.com>
2023-12-12 17:50:08 +0100

    StandardResultsSetPagination

--- apps/shop/paginators.py
+++ apps/shop/paginators.py
@@ -1,19 +1,7 @@
 from rest_framework.pagination import PageNumberPagination
 
 
-class LargeResultsSetPagination(PageNumberPagination):
-    page_size = 100
-    page_size_query_param = "page_size"
-    max_page_size = 1000
-
-
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = "page_size"
     max_page_size = 36
-
-
-class SmallResultsSetPagination(PageNumberPagination):
-    page_size = 2
-    page_size_query_param = "page_size"
-    max_page_size = 20
