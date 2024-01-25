b21b49e22893a4f823ef8d3d690b9e3828066a89
AlexUA_ubuntu <alex2011ua@gmail.com>
2024-01-25 07:31:14 +0100

    url

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('api/shop/', include('apps.shop.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('', SpectacularSwaggerView.as_view(url_name='schema'))
+    path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
+    path('', SpectacularSwaggerView.as_view())
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
