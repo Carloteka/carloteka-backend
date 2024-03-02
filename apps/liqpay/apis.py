2bb8644603764d6defb7cf43ae882584e7ff3ddd
AlexUA_ubuntu <alex2011ua@gmail.com>
2024-03-02 21:15:32 +0100

    add endpoint for getting status bill payment

--- apps/liqpay/apis.py
+++ apps/liqpay/apis.py
@@ -5,7 +5,7 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from drf_spectacular.utils import OpenApiParameter, extend_schema
 from rest_framework import serializers
-from rest_framework.exceptions import NotAcceptable
+from rest_framework.exceptions import NotAcceptable, NotFound
 from rest_framework.views import APIView, Response
 
 from apps.core.exceptions import ErrorSerializer404
@@ -27,6 +27,7 @@ class PayApi(APIView):
 
     @extend_schema(
         tags=["liqpay"],
+        summary="Get params for link",
         responses={200: OutputPayApiSerializer(), 404: ErrorSerializer404()},
         parameters=[
             OpenApiParameter(
@@ -69,8 +70,8 @@ class PayApi(APIView):
 
         data = liq_pay.cnb_data(params)
         signature = liq_pay.cnb_signature(params)
-        return Response({"data": data, "signature": signature})
-        # return render(request,"test_liqpay_button.html", {"data": data, "signature": signature})
+        # return Response({"data": data, "signature": signature})
+        return render(request, "test_liqpay_button.html", {"data": data, "signature": signature})
 
 
 @method_decorator(csrf_exempt, name="dispatch")
@@ -103,3 +104,164 @@ class PayCallbackApi(APIView):
                 response["order_id"], "error", response["acq_id"]
             )
         return Response(200)
+
+
+class GetLiqPayStatusApi(APIView):
+    """Return status of liqpay payment"""
+
+    class InputGetLiqPayStatusApi(serializers.Serializer):
+        order_id = serializers.IntegerField()
+
+    class OutputGetLiqPayStatusApi(serializers.Serializer):
+        action = serializers.CharField(max_length=50)
+        payment_id = serializers.IntegerField()
+        status = serializers.CharField(max_length=50)  # must be success
+        paytype = serializers.CharField(max_length=50)
+        acq_id = serializers.IntegerField()
+        sender_phone = serializers.CharField(max_length=50)
+        description = serializers.CharField(max_length=250)
+        amount = serializers.FloatField()
+
+    class OutputErrorApiSerializer(serializers.Serializer):
+        err_code = serializers.CharField(max_length=255, required=False)
+        status = serializers.CharField(max_length=50)  # must be success
+        err_description = serializers.CharField(max_length=255)
+
+    @extend_schema(
+        tags=["liqpay"],
+        summary="Get status of liqpay payment",
+        description="200 - the bill is paid, 402 - the bill has not been paid",
+        responses={200: OutputGetLiqPayStatusApi(),
+                   402: OutputErrorApiSerializer(),
+                   404: ErrorSerializer404()},
+        parameters=[
+            OpenApiParameter(
+                name="order_id",
+                location=OpenApiParameter.QUERY,
+                description="order_id",
+                required=True,
+                type=int,
+            ),
+        ],
+    )
+    def get(self, request, *args, **kwargs):
+        serializer = self.InputGetLiqPayStatusApi(data=request.query_params)
+        serializer.is_valid(raise_exception=True)
+        params = {
+            "action": "status",
+            "version": "3",
+            "order_id": serializer.validated_data["order_id"],
+        }
+        data = liq_pay.api("request", params)
+        if data["status"] == "error":
+            raise NotFound(data["err_description"])
+
+        if data["status"] == "success":
+            output_serializer = self.OutputGetLiqPayStatusApi(data=data)
+            output_serializer.is_valid()
+            return Response(data=output_serializer)
+        # not payed
+        output_serializer = self.OutputErrorApiSerializer(data=data)
+        output_serializer.is_valid()
+        return Response(data=output_serializer.validated_data, status=402)
+
+"""
+order not found:
+{
+  "code": "payment_not_found",
+  "err_code": "payment_not_found",
+  "err_description": "Платеж не найден",
+  "result": "error",
+  "status": "error"
+}
+Не успішна оплата. Код помилки - limit:
+{
+  "result": "ok",
+  "payment_id": 2432611415,
+  "action": "pay",
+  "status": "try_again",
+  "err_code": "limit",
+  "err_description": "Limit is exceeded",
+  "version": 3,
+  "type": "buy",
+  "paytype": "card",
+  "public_key": "sandbox_i55528694126",
+  "acq_id": 414963,
+  "order_id": "9",
+  "liqpay_order_id": "VOHXV7551709408712492250",
+  "description": "second",
+  "sender_first_name": "sdf",
+  "sender_last_name": "fsdf",
+  "sender_card_mask2": "400000*02",
+  "sender_card_bank": "Sandbox",
+  "sender_card_type": "visa",
+  "sender_card_country": 804,
+  "ip": "47.62.166.91",
+  "amount": 1000,
+  "currency": "UAH",
+  "sender_commission": 0,
+  "receiver_commission": 15,
+  "agent_commission": 0,
+  "amount_debit": 1000,
+  "amount_credit": 1000,
+  "commission_debit": 0,
+  "commission_credit": 15,
+  "currency_debit": "UAH",
+  "currency_credit": "UAH",
+  "sender_bonus": 0,
+  "amount_bonus": 0,
+  "mpi_eci": "7",
+  "is_3ds": false,
+  "language": "uk",
+  "create_date": 1709408712494,
+  "end_date": 1709408712612,
+  "transaction_id": 2432611415,
+  "code": "limit"
+}
+
+Недостатньо коштів
+{
+  "result": "ok",
+  "payment_id": 2432611415,
+  "action": "pay",
+  "status": "try_again",
+  "err_code": "9859",
+  "err_description": "Insufficient funds",
+  "version": 3,
+  "type": "buy",
+  "paytype": "card",
+  "public_key": "sandbox_i55528694126",
+  "acq_id": 414963,
+  "order_id": "9",
+  "liqpay_order_id": "YVSEJ0JA1709409249516739",
+  "description": "second",
+  "sender_first_name": "fd",
+  "sender_last_name": "vdfd",
+  "sender_card_mask2": "400000*95",
+  "sender_card_bank": "Sandbox",
+  "sender_card_type": "visa",
+  "sender_card_country": 804,
+  "ip": "47.62.166.91",
+  "amount": 1000,
+  "currency": "UAH",
+  "sender_commission": 0,
+  "receiver_commission": 15,
+  "agent_commission": 0,
+  "amount_debit": 1000,
+  "amount_credit": 1000,
+  "commission_debit": 0,
+  "commission_credit": 15,
+  "currency_debit": "UAH",
+  "currency_credit": "UAH",
+  "sender_bonus": 0,
+  "amount_bonus": 0,
+  "mpi_eci": "7",
+  "is_3ds": false,
+  "language": "uk",
+  "create_date": 1709408712494,
+  "end_date": 1709409249753,
+  "transaction_id": 2432611415,
+  "code": "9859"
+}
+
+"""
